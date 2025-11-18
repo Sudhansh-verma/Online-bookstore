8ae0996fa6a778b60833e8d7e5e41f4bd84fa7a5
Sudhansh-verma <sudhanshverma004@gmail.com>
2025-11-18 19:48:18 +0530

    Create Bookcontroller.php

---
+++ app/http/Bookcontroller.php
@@ -0,0 +1,11 @@
+namespace App\Http\Controllers;
+use App\Models\Book;
+
+class BookController extends Controller
+{
+    public function index()
+    {
+        $books = Book::all();
+        return view('books', compact('books'));
+    }
+}
