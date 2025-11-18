e29f2bfca274f42221e0ff5a8f4300c97adb92a5
Sudhansh-verma <sudhanshverma004@gmail.com>
2025-11-18 19:42:50 +0530

    Create chechout.blade.php

---
+++ resources/views/chechout.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container mt-4">
+  <h2>Checkout Successful!</h2>
+  <p>Thank you for your purchase.</p>
+  <a href="/" class="btn btn-primary">Continue Shopping</a>
+</div>
+@endsection
